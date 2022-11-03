f42183a080c714b77b861366dd8f1191b4024343
Siti Iedrania <13519137@std.stei.itb.ac.id>
2022-11-03 20:07:49 +0700

    feat: integrate model to app

--- deploy-mlmodel/deploy-mlmodel-project/app.py
+++ deploy-mlmodel/deploy-mlmodel-project/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, request, render_template
+import pickle
 
 app = Flask(__name__)
 
@@ -9,12 +10,23 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-    # bulan = request.form["bulan"]
-    # tahun = int(request.form["tahun"])
-    # kualitas = (request.form["kualitas"])
-    # prediction = model.predict([[bulan, tahun, kualitas]])
-    # output = round(prediction[0],2)
-        prediction = 5000 # Nilai prediksi diisi di sini
+        bulan = request.form["bulan"]
+        tahun = int(request.form["tahun"])
+        kualitas = (request.form["kualitas"])
+        # prediction = model.predict([[bulan, tahun, kualitas]])
+
+        if (kualitas == "premium"):
+            kualitas = 0
+        elif (kualitas == "medium"):
+            kualitas = 1
+        elif (kualitas == "luar_kualitas"):
+            kualitas = 2
+
+        pickled_model = pickle.load(open('model/model.pkl', 'rb'))
+        result = pickled_model.predict([[tahun, bulan, kualitas]])
+
+        output = round(result[0],2)
+        prediction = output # Nilai prediksi diisi di sini
         return render_template('index.html', prediction=("Prediksi harga beras: Rp " + str(prediction)))
 if __name__ == "__main__":
     app.run()
