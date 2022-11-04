3f619499d50cc8cdccb6a2562ffb7fb98af8cea0
rheael <rpandumpi@gmail.com>
2022-11-04 07:28:07 +0700

    edit result sentence

--- app.py
+++ app.py
@@ -10,23 +10,36 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-        bulan = request.form["bulan"]
+        bulan = int(request.form["bulan"])
         tahun = int(request.form["tahun"])
-        kualitas = (request.form["kualitas"])
-        # prediction = model.predict([[bulan, tahun, kualitas]])
+        kualitas = int(request.form["kualitas"])
 
-        if (kualitas == "premium"):
-            kualitas = 0
-        elif (kualitas == "medium"):
-            kualitas = 1
-        elif (kualitas == "luar_kualitas"):
-            kualitas = 2
+        kualitas_value = {
+            0: "Premium",
+            1: "Medium",
+            2: "Luar Kualitas",
+        }
+
+        bulan_value = {
+            0: "Januari",
+            1: "Februari",
+            2: "Maret",
+            3: "April",
+            4: "Mei",
+            5: "Juni",
+            6: "Juli",
+            7: "Agustus",
+            8: "September",
+            9: "Oktober",
+            10: "November",
+            11: "Desember"
+        }
 
         pickled_model = pickle.load(open('model/model.pkl', 'rb'))
         result = pickled_model.predict([[tahun, bulan, kualitas]])
 
         output = round(result[0],2)
         prediction = output # Nilai prediksi diisi di sini
-        return render_template('index.html', prediction=("Prediksi harga beras: Rp " + str(prediction)))
+        return render_template('index.html', first_line=("Prediksi harga beras " + kualitas_value[kualitas]), second_line=(" Bulan " + bulan_value[bulan] + " " + str(tahun) + " Rp " + str(prediction)))
 if __name__ == "__main__":
     app.run()
